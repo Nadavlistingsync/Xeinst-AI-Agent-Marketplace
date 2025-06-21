af3318e096ec4ecffc8db9d7391227fa86dc9e5d
Nadavlistingsync <Benedeknadav@gmail.com>
2025-06-21 10:12:32 +0200

    fix(marketplace): use default export for AgentDetails

--- src/components/marketplace/AgentDetails.tsx
+++ src/components/marketplace/AgentDetails.tsx
@@ -12,7 +12,7 @@ interface AgentDetailsProps {
   deployment: Deployment;
 }
 
-export function AgentDetails({ deployment }: AgentDetailsProps) {
+export default function AgentDetails({ deployment }: AgentDetailsProps) {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
       <div className="lg:col-span-2 space-y-8">
