c705b99ea9bc0f62a4708e7ecdd96ff102e7a5a0
Nadavlistingsync <Benedeknadav@gmail.com>
2025-06-24 09:15:35 -0400

    Fix AIInterfaceGenerator props error - Remove agentId and onInterfaceGenerated props - Component is now standalone and doesn't require these props

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -288,10 +288,7 @@ export default function UploadPage() {
 
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
             <div className="lg:col-span-2">
-              <AIInterfaceGenerator 
-                agentId={uploadedAgentId} 
-                onInterfaceGenerated={handleInterfaceGenerated}
-              />
+              <AIInterfaceGenerator />
             </div>
 
             <div className="space-y-6">
